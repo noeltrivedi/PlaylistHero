98d190aa2463e62b65378150783f7a671e81f89d
Vinson Compestine <vcompestine@gmail.com>
2015-11-14 15:42:26 -0800

    Voting works!

--- app/src/main/java/itp341/compestine/vinson/playlisthero/SongAdapter.java
+++ app/src/main/java/itp341/compestine/vinson/playlisthero/SongAdapter.java
@@ -8,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -16,6 +17,8 @@ import java.util.ArrayList;
  */
 public class SongAdapter extends ArrayAdapter<Song> {
     Song song;
+    int upclick =0;
+    int downclick = 0;
 
     public SongAdapter(Context context, ArrayList<Song> songs ){super (context, 0, songs);}
     public View getView(int position, View convertView, ViewGroup parent){
@@ -28,30 +31,50 @@ public class SongAdapter extends ArrayAdapter<Song> {
         TextView tvName = (TextView)convertView.findViewById(R.id.songName);
         TextView tvArtist = (TextView)convertView.findViewById(R.id.artistName);
         ImageView tvAlbum = (ImageView)convertView.findViewById(R.id.songImage);
-        TextView tvVotes = (TextView)convertView.findViewById(R.id.votes);
+        final TextView tvVotes = (TextView)convertView.findViewById(R.id.votes);
 
         tvName.setText(song.getName());
         tvArtist.setTag(song.getArtist());
         tvAlbum.setImageDrawable(song.getSongArt());
         tvVotes.setText(song.getVotes());
 
-        ImageButton upVote = (ImageButton)convertView.findViewById(R.id.upVote);
-        ImageButton downVote = (ImageButton)convertView.findViewById(R.id.downVote);
-
+        ImageView upVote = (ImageView)convertView.findViewById(R.id.upVote);
         upVote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                song.upVote();
+                //Checks if user has already downvoted
+                if(downclick != 0 && upclick == 0){
+                    song.upVote();
+                    upclick =0;
+                    downclick = 0;
+                }
+
+               else if(upclick  == 0){
+                    song.upVote();
+                    upclick+=1;
+                }
+                tvVotes.setText(song.getVotes());
             }
         });
 
+        ImageView downVote = (ImageView)convertView.findViewById(R.id.downVote);
         downVote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                song.downVote();
+
+                if(upclick != 0 && downclick == 0){
+                    song.downVote();
+                    downclick =0;
+                    upclick = 0;
+                }
+
+                else if (downclick == 0){
+                    song.downVote();
+                    downclick +=1;
+                }
+                tvVotes.setText(song.getVotes());
             }
         });
-
 
         return convertView;
     }
