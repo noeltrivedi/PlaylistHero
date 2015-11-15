30d60d75bdd5deafc20d3cc0023ebcd20b1a903a
Vinson Compestine <vcompestine@gmail.com>
2015-11-14 18:45:07 -0800

    edittext

--- app/src/main/java/itp341/compestine/vinson/playlisthero/DJActivity.java
+++ app/src/main/java/itp341/compestine/vinson/playlisthero/DJActivity.java
@@ -4,25 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
 
-public class AddSong extends Activity {
+public class DJActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_song);
-
-        String suggestion;
-        EditText searchBar = (EditText)findViewById(R.id.searchBar);
-        suggestion = searchBar.getText().toString(); 
-
+        setContentView(R.layout.activity_dj);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_add_song, menu);
+        getMenuInflater().inflate(R.menu.menu_dj, menu);
         return true;
     }
 
